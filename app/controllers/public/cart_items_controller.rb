3f3b178b1b110fd4db181529a8bc1058ef3ff38e
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-07-14 12:17:06 +0000

    [Add]publicコントローラー追加

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,16 @@
+class Public::CartItemsController < ApplicationController
+  def index
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def delete_all
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,54 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/create'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/delete'
+    get 'cart_items/delete_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdrawal'
+  end
+  namespace :public do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :publics, skip: [passwords], controllers: {
+  devise_for :public, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
