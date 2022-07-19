a6c9461859e965f1b8f6d475fa27ce048af4077d
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-07-19 12:21:27 +0000

    [Add]ルート編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,12 @@
 Rails.application.routes.draw do
+  
   namespace :admin do
-    get 'order_items/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
+    get "/" => "homes/top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_items, only: [:update]
   end
   namespace :admin do
     get 'sessions/new'
