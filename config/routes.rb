2f18e37d896f3463da32a962a0f70c076fca7994
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-07-21 13:33:41 +0000

    [Add]ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,36 @@
 Rails.application.routes.draw do
-  
-  namespace :admin do
-    get "/" => "homes/top"
+ namespace :admin do
+    get "/" => "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_items, only: [:update]
   end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
+
+  scope module: :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:show, :edit, :update]
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdrawal' => 'customers#withdrawal'
+    delete '/cart_items/delete_all' => 'cart_items#delete_all'
+    delete '/cart_items/:id' => 'cart_items#delete'
+    resources :cart_items, only: [:index, :update, :delete, :create]
+    get '/orders/thanks' => 'orders#thanks'
+    post '/orders/confirm' => 'orders#confirm'
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/create'
-    get 'orders/thanks'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/delete'
-    get 'cart_items/delete_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdrawal'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
+
   devise_for :public, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
