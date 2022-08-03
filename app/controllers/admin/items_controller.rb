244e59eaec807569c89594a8ae6d1853cfa30cb9
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-08-03 13:38:59 +0000

    [Add]商品新規登録画面追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,5 +22,10 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
