5b2f64107d760f7bbace02a331391f9fac591928
natusmikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-08-07 09:08:01 +0000

    [Add]商品詳細画面追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b1585e64eee1fe8968a92016b8bcda4f2b5b2c0ea059b52dff0167ee5ded68407b98bee965ff391b248c66f82a648d3875950232cb40bcd601b5c92c3a017614'
